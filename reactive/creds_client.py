94588cf6ec5612c4349d4954304063eac92039ce
David Ames <david.ames@canonical.com>
2018-06-05 18:15:07 +0000

    Render

--- reactive/creds_client.py
+++ reactive/creds_client.py
@@ -1,4 +1,7 @@
 from charms.reactive import when, when_not, set_state
+import charms.reactive
+
+DOMAIN = "kubernetes"
 
 @when('keystone-credentials.connected')
 @when_not('keystone-credentials.available.auth')
@@ -6,10 +9,47 @@ def request_creds(keystone):
     keystone.request_credentials(
         "kubadmin",
         "kubinfra",
-        requested_grants="Admin"
-        domain="kubernetes")
+        requested_grants="Admin",
+        domain=DOMAIN)
 
 
-@when('keystone-credentials.available.auth')
+@when('identity-crentials.available.auth')
 def render(keystone):
-    pass
+    print("RECEIVED")
+    print(dir(keystone))
+    clean ="""_OS_PARAMS=$(env | awk 'BEGIN {FS="="} /^OS_/ {print $1;}' | paste -sd ' ')
+    for param in $_OS_PARAMS; do
+        unset $param
+    done
+    unset _OS_PARAMS
+    """
+
+    creds = """
+    export OS_REGION_NAME=RegionOne
+    export OS_AUTH_URL=http://{}:{}/v3
+    export OS_USERNAME={}
+    export OS_PASSWORD={}
+    export OS_PROJECT_NAME={}
+    export OS_PROJECT_DOMAIN_NAME={}
+    export OS_USER_DOMAIN_NAME={}
+    export OS_IDENTITY_API_VERSION={}
+    export OS_AUTH_VERSION={}
+    """
+    rc = clean + creds.format(
+        keystone.credentials_host(),
+        keystone.credentials_port(),
+        keystone.credentials_username(),
+        keystone.credentials_password(),
+        keystone.credentials_project(),
+        DOMAIN,
+        DOMAIN,
+        keystone.api_version(),
+        keystone.api_version())
+    print(rc)
+
+
+@when_not('installed')
+def debug():
+    print("DEBUG")
+    #print(dir(charms.reactive.flags))
+    print(charms.reactive.flags.get_flags())
